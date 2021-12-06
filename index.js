93ea0506c2dfb1ba1d0ec51e82d6f944a028b477
Gleb Kletksov <gfgfddgleb@gmail.com>
2021-12-06 23:42:24 +0300

    first version

---
+++ index.js
@@ -0,0 +1,48 @@
+const fs = require('fs');
+const { argv } = require('process');
+
+const buildFilePath = () => {
+    const defaultPath = `${process.cwd()}/package.json`;
+    let basePath = argv[3] || defaultPath;
+
+    // Check that provided path is dir
+    try {
+        if (fs.lstatSync(basePath).isDirectory()) {
+            if (basePath.endsWith('/')) {
+                basePath += 'package.json';
+            } else {
+                basePath += '/package.json';
+            }
+        } 
+    } catch (error) {
+        console.log("ERROR")
+        console.log(`PATH ${basePath} DONT EXISTS`);
+        process.exit(1);
+    }
+
+    // Check that provided file exists
+    const isExists = fs.existsSync(basePath);
+
+    if (isExists && basePath.endsWith('package.json')) {
+        return basePath;
+    } else {
+        console.log("ERROR");
+        console.log(`PROVIDED PATH ${basePath} DONT EXISTS OR ITS NOT A package.json file`);
+        process.exit(1);
+    }
+}
+
+const file = buildFilePath();
+
+const logResult = (filePath) => {
+    if (argv[2]) {
+        const result = JSON.parse(fs.readFileSync(filePath, 'utf-8'));
+        console.dir(result[argv[2]]);
+    } else {
+        console.log("ERROR");
+        console.log("FIELD WAS NOT PROVIDED");
+        process.exit(1);
+    }
+}
+
+logResult(file);
